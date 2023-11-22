8945fa0bd113020a5b27527229699db094f2ea1d
Dennis Moldovan <dennismoldovan32@gmail.com>
2023-11-22 20:45:29 +0200

    server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,23 @@
-const OraganizationIdPage = () => {
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import Board from "./board";
+import { Form } from "./form";
+
+
+
+const OraganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  
 
   return (
-    <div>
-      <form>
-        <input id="title" name="title" required placeholder="Enter a board title" className="border-black border p-1"/>
-      </form>
+    <div className="flex flex-col space-y-4">
+      <Form/>
+      <div className="space-y-2">
+        {boards.map((board)=>(
+          <Board key={board.id} title={board.title} id={board.id}/>
+        ))}
+      </div>
     </div>
   )
 }
